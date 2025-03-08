e370e60f69e85fe9281c95d5b0371eb5ad923bc7
Akash Dhiman <akashdhiman@Akashs-MacBook-Air.local>
2025-03-08 16:39:19 +0530

    Server side rendering added using EJS

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { connectToMongoDB } = require('./connect')
 const urlRoute = require('./routes/url')
+const staticRoute = require('./routes/staticRoute')
+const path = require('path')
 const URL = require('./models/url');
 
 const app = express();
@@ -9,10 +11,45 @@ const PORT = 8001;
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
 .then (()=> console.log("MongoDB connected"))
 
-app.use(express.json())
-app.use("/url" , urlRoute)
+app.set("view engine" , "ejs")
+app.set("views" , path.resolve("./views"))
 
-app.get("/:shortId", async (req, res) => {
+app.use(express.json())
+app.use(express.urlencoded({extended:false})) //for form data
+
+app.use("/url" , urlRoute)
+app.use("/" , staticRoute)
+
+// Server Side Rendering -> 
+// Write html on server side -> complicated
+// For ease we use EJS
+app.get('/test' , async(req , res)=>{
+  const allUrls = await URL.find({});
+  // return res.end(`
+  //   <html>
+  //     <head></head>
+  //     <body>
+  //       <ol>
+  //         ${allUrls.map( url => `<li>
+  //           ${url.shortId} - 
+  //           ${url.redirectURL} - 
+  //           ${url.visitHistory.length}</li>`
+  //           ).join("")
+  //           }
+  //       </ol>
+  //     </body>
+  //   </html>`
+    
+  // )
+  
+  return res.render('home',{
+    urls: allUrls,
+  })
+})
+
+
+
+app.get("/url/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
       {
